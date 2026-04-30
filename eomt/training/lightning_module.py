9352816ef17e6dc8a3ccfbe1a8b9154426567737
s363752 <miriage96@gmail.com>
2026-04-30 23:35:51 +0200

    Skip frozen params in configure_optimizers

--- eomt/training/lightning_module.py
+++ eomt/training/lightning_module.py
@@ -118,6 +118,8 @@ class LightningModule(lightning.LightningModule):
         ).tolist()
 
         for name, param in reversed(list(self.named_parameters())):
+            if not param.requires_grad:
+                continue
             lr = self.lr
 
             if name.replace("network.encoder.backbone.", "") in encoder_param_names:
